6ab2dbaefd88edb214030e917fd587a8ad5f32fa
Greg Wilson <gvwilson@third-bit.com>
2023-12-30 08:59:48 -0500

    Better plot

--- bin/status.py
+++ bin/status.py
@@ -1,5 +1,5 @@
 import argparse
-from datetime import timedelta
+from datetime import datetime, timedelta
 import numpy as np
 import pandas as pd
 import plotly.express as px
@@ -10,30 +10,48 @@ import sys
 def main():
     options = parse_args()
 
+    # Load data.
     info = pd.read_csv(options.info)
     status = pd.read_csv(options.status).groupby("story").filter(lambda x: len(x) > 1)
-    status = status.sort_values("date", ascending=False)
+    status = status.sort_values("date")
 
+    # Find word counts of active stories and show current totals.
     df = status.set_index("story")\
                .join(info.set_index("story"))\
                .sort_values(["title", "date"])\
                .reset_index()\
                [["title", "date", "words"]]
     df["date"] = pd.to_datetime(df["date"]).dt.date
-    print(df.drop_duplicates(["title"]).to_markdown(index=False))
+    print(df.drop_duplicates(["title"], keep="last").to_markdown(index=False))
 
+    # Find change over time.
     df["change"] = df.groupby("title").diff()["words"]
     change = df.dropna()[["date", "change"]].groupby(["date"]).sum("change").reset_index()
     change = change[change["change"] != 0]
     change = change.rename(columns={"change": "words"})
     change["title"] = "change"
 
-    df = pd.concat([df[["title", "date", "words"]], change])
+    # Fill in missing dates.
+    known = set(change["date"])
+    min_date = min(change["date"])
+    max_date = max(change["date"])
+    full = {min_date + timedelta(days=d) for d in range(0, (max_date - min_date).days)}
+    missing = pd.DataFrame({"title": "change", "words": 0, "date": list(sorted(full - known))})
+    change = pd.concat([change, missing]).sort_values("date").reset_index()
+    print(change[["date", "words"]].tail(n=7).to_markdown(index=False))
 
-    fig = px.line(df, x="date", y="words", color="title", line_shape="hv")
+    # Combine stories and changes for plotting.
+    df["kind"] = "story"
+    change["kind"] = "delta"
+    df = pd.concat([df[["title", "date", "words", "kind"]], change])
+
+    # Plot.
+    fig = px.line(df, x="date", y="words", color="title", line_shape="hv", facet_row="kind")
     date_offset = timedelta(days=1)
-    fig.update_xaxes(range=[min(df["date"]) - date_offset, max(df["date"]) + date_offset])
-    fig.show()
+    fig.update_xaxes(range=[min_date - date_offset, max_date + date_offset])
+    fig.update_yaxes(matches=None)
+    if options.show:
+        fig.show()
     fig.write_image(options.chart, width=1200)
 
 
@@ -42,6 +60,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("--chart", required=True, help="Chart filename")
     parser.add_argument("--info", required=True, help="Information about stories")
+    parser.add_argument("--show", action="store_true", help="Show plot")
     parser.add_argument("--status", required=True, help="Status information")
     return parser.parse_args()
 
