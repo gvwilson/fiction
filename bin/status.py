a93c44a9729e9839f572375a958e6b46cf8327e3
Greg Wilson <gvwilson@third-bit.com>
2024-01-06 19:20:15 -0500

    Better progress display

--- bin/status.py
+++ bin/status.py
@@ -15,27 +15,34 @@ def main():
 
     # Load data.
     info = pd.read_csv(options.info)
-    status = pd.read_csv(options.status).groupby("story").filter(lambda x: len(x) > 1)
-    status = status.sort_values("date")
+    status = pd.read_csv(options.status)\
+               .groupby("story")\
+               .filter(lambda x: len(x) > 1)\
+               .sort_values("date")
 
     # Find word counts of active stories and show current totals.
     df = status.set_index("story")\
                .join(info.set_index("story"))\
                .sort_values(["title", "date"])\
                .reset_index()\
-               [["title", "date", "words"]]
+               [["title", "date", "words", "category"]]
     df["date"] = pd.to_datetime(df["date"]).dt.date
     start = df.drop_duplicates(["title"], keep="first")\
               .rename(columns={"date": "start", "words": "initial"})\
               [["title", "initial", "start"]]
     current = df.drop_duplicates(["title"], keep="last")
-    combined = start.set_index("title").join(current.set_index("title")).reset_index()
-    combined["diff"] = combined["words"] - combined["initial"]
-    print(combined[["title", "date", "words", "diff"]].to_markdown(index=False))
+    summary = start.set_index("title").join(current.set_index("title")).reset_index()
+    summary["diff"] = summary["words"] - summary["initial"]
+    print(summary[["title", "date", "words", "diff"]].to_markdown(index=False))
 
     # Find change over time.
-    df["change"] = df.groupby("title").diff()["words"]
-    change = df.dropna()[["date", "change"]].groupby(["date"]).sum("change").reset_index()
+    df["change"] = df[["title", "date", "words"]]\
+                     .groupby("title")\
+                     .diff()["words"]
+    change = df.dropna()[["date", "change"]]\
+               .groupby(["date"])\
+               .sum("change")\
+               .reset_index()
     change = change[change["change"] != 0]
     change = change.rename(columns={"change": "words"})
     change["title"] = "change"
@@ -51,21 +58,21 @@ def main():
     print(f'\n{change[["date", "words"]].tail(n=WEEK).to_markdown(index=False)}')
 
     # Show overall rate.
-    print(f'\nrate: {int(sum(combined["diff"]) / num_days)} words/day')
+    print(f'\nrate: {int(sum(summary["diff"]) / num_days)} words/day')
 
     # Combine stories and changes for plotting.
-    df["kind"] = "story"
-    change["kind"] = "delta"
-    df = pd.concat([df[["title", "date", "words", "kind"]], change])
+    change["category"] = "delta"
+    df = pd.concat([df[["title", "date", "words", "category"]], change])
 
     # Plot.
-    fig = px.line(df, x="date", y="words", color="title", line_shape="hv", facet_row="kind")
+    fig = px.line(df, x="date", y="words", color="title", line_shape="hv", facet_row="category")
     date_offset = timedelta(days=1)
     fig.update_xaxes(range=[min_date - date_offset, max_date + date_offset])
     fig.update_yaxes(matches=None)
+    fig.for_each_annotation(lambda a: a.update(text=a.text.split("=")[-1]))
     if options.show:
         fig.show()
-    fig.write_image(options.chart, width=1200)
+    fig.write_image(options.chart, width=800, height=800)
 
 
 def parse_args():
